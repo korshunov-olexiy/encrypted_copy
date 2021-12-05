6d35720c76a24f80e3592aaba46589e9772c13f0
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-12-05 10:09:34 +0200

    main

---
+++ mindmap/map_encrypting.mm
@@ -0,0 +1,88 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1638681558723" ID="ID_1311989154" MODIFIED="1638684863743">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Copy <b>dir</b>&#160;or <b>file</b>&#160;or <b>list of files</b>
+    </p>
+    <p>
+      with encrypting
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="gohome"/>
+<node CREATED="1638681800968" ID="ID_130920061" MODIFIED="1638683514119" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      get command line <b>arguments</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<icon BUILTIN="kaddressbook"/>
+<node COLOR="#990000" CREATED="1638682104641" ID="ID_192886485" MODIFIED="1638682569589">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      get <b><font color="#009966">first arg</font></b>: input dir or file/list of files
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#990000" CREATED="1638682186677" ID="ID_1669163964" MODIFIED="1638682587996">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      get <font color="#009966"><b>second arg</b></font>: output dir
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#990000" CREATED="1638682365701" ID="ID_189715204" MODIFIED="1638682590716">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      get <b><font color="#009966">third arg</font></b>: password
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1638683374369" ID="ID_47059035" MODIFIED="1638683603536" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      get a <b><font color="#cc0033">list</font></b>&#160;of files to encrypt
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1638683634048" ID="ID_1695987216" MODIFIED="1638683634048" TEXT=""/>
+</node>
+</node>
+</map>
